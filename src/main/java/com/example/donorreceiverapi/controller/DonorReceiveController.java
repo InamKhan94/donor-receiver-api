93f460adb2e2f50b6e83f6566ac0025c48c97018
Inam Ullah Khan <inam.ullah@backcountry.com>
2022-10-19 16:14:17 +0500

    Donor Receiver api

--- src/main/java/com/example/donorreceiverapi/controller/DonorReceiveController.java
+++ src/main/java/com/example/donorreceiverapi/controller/DonorReceiveController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,7 +24,8 @@ public class DonorReceiveController {
 
     @PostMapping(value = "/create-donor-receiver")
     //,@RequestParam(value="Prescription", required = false) MultipartFile prescription
-    public String createDonorReceiver(@ModelAttribute("donorReceiverDTO") DonorReceiverDTO donorReceiverDTO) {
+   // @ModelAttribute("donorReceiverDTO")
+    public String createDonorReceiver(@RequestBody DonorReceiverDTO donorReceiverDTO) {
         donorReceiverService.createDonorReceiver(donorReceiverDTO);
         return "redirect:/";
     }
